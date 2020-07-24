e93acea02d0bcbbf67ddaa5bb13354dccfcb0362
T-baliang <756345008@qq.com>
2020-07-25 00:04:31 +0800

    第一次向github提交代码

---
+++ demo1.py
@@ -0,0 +1,40 @@
+
+# print("hello world!")   #字符串
+# print(True)           #布尔值
+# print(2333)       #整数
+# print(2.22)       #小数
+# print(())        #元祖
+# print([])         # 数组
+# print({})           #字典
+# print("hello world!",2333)
+# print("哈哈"+"嘻嘻") #字符串带拼接，只能相同的数据类型
+# print("哈哈"*10)
+# print(1+2)
+# print(2>3)
+# print(2<3)
+
+# a="张三"   #把张三这个值赋值给了名字叫a的这个变量，变量必须是小写字母
+# print(a)
+
+# a=float(input("请输入："))
+# b=float(input("请输入："))
+# print("input获取的内容:",a+b)
+
+# print(type("哈哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+# a=int("2333")
+# print(type(a))
+
+# a="asdlkfjksdjf  aldkjfksldjf"
+# print(len(a))
+
+a=input("请输入：")
+b=input("请输入：")
+print("长度=",len(a+b))
